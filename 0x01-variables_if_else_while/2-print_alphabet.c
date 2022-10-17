4f4ff8278f0961bf30b33ba446c734bc5363f32e
Godwin Charity Ezinne <godwincharity01@gmail.com>
2022-10-17 14:37:52 -0700

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - starting point
+*
+* Description: 'Main program'
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
