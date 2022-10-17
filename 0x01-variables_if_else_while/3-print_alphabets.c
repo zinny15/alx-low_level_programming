51f3963e85c09b5cd20e2dee0550769a09f32175
Godwin Charity Ezinne <godwincharity01@gmail.com>
2022-10-17 14:47:58 -0700

    AtoZ

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - starting point
+*
+* Description: 'Main program'
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
