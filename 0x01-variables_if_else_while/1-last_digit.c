ac7cf74052ca535a2873b7ec64669f969d1069c6
Godwin Charity Ezinne <godwincharity01@gmail.com>
2022-10-17 14:27:58 -0700

    second

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int ldigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-idigit = n % 10;
+ldigit = n % 10;
 if (ldigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, ldigit);
